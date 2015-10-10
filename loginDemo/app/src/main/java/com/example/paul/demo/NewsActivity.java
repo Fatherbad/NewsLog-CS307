0401563b1e9422c1064a9d287093afa31d862502
Fatherbad <smotherw@purdue.edu>
2015-10-10 16:55:05 -0400

    Swipe feature added to News Activitiy

--- loginDemo/app/src/main/java/com/example/paul/demo/NewsActivity.java
+++ loginDemo/app/src/main/java/com/example/paul/demo/NewsActivity.java
@@ -1,7 +1,12 @@
 package com.example.paul.demo;
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.os.Bundle;
@@ -13,10 +18,12 @@ import android.widget.Button;
  */
 public class NewsActivity extends AppCompatActivity {
     
-    
-     
+    private GestureDetectorCompat mDetector;
+    private static final float SWIPE_THRESHOLD = 100;
+    private float x1, x2, y1, y2;
     private WebView webView;
-    
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +35,36 @@ public class NewsActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
 
         webView.setWebViewClient(new Callback());
-        webView.loadUrl("http://www.google.com");
+        webView.loadUrl("http://www.nytimes.com/2015/10/07/technology/the-hardware-side-of-microsoft-unveils-a-pile-of-new-devices.html");
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        x1 = event.getX();
+                        y1 = event.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        x2 = event.getX();
+                        y2 = event.getY();
 
-//        String customHtml = "<html><body><h1>Hello, WebView</h1></body></html>";
-//        webView.loadData(customHtml, "text/html", "UTF-8");
-
+                        float diffX = x1 - x2;
+                        float diffY = y1 - y2;
+                        if (Math.abs(diffX) > Math.abs(diffY) + 10 && Math.abs(x1-x2) > SWIPE_THRESHOLD) {
+                            if (x1 > x2) {
+                                onSwipeRight();
+                            } else onSwipeLeft();
+                            
+                            return true;
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
+    
     private class Callback extends WebViewClient {
 
         @Override
@@ -42,5 +72,14 @@ public class NewsActivity extends AppCompatActivity {
             return (false);
         }
     }
+    
 
+    public void onSwipeRight() {
+        webView.loadUrl("http://www.nytimes.com/2015/10/07/business/international/vw-diesel-emissions-job-cuts.html");
+
+    }
+
+    public void onSwipeLeft() {
+        webView.loadUrl("http://www.nytimes.com/aponline/2015/10/06/world/asia/ap-as-skorea-earns-samsung-electronics-.html");
+    }
 }
