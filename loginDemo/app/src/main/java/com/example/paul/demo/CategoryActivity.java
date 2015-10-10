cabde6cb969d1e74f1420e210dcc8a0780416cb1
Joseph R. Nathan <nathan@pod2-4.cs.purdue.edu>
2015-10-10 13:42:32 -0400

    sports button links to news activity

--- loginDemo/app/src/main/java/com/example/paul/demo/CategoryActivity.java
+++ loginDemo/app/src/main/java/com/example/paul/demo/CategoryActivity.java
@@ -1,8 +1,10 @@
 package com.example.paul.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -14,18 +16,26 @@ import android.widget.TextView;
  */
 public class CategoryActivity extends AppCompatActivity {
 
-    public Button SportsButton;
-    public Button PoliticsButton;
-    public Button TechButton;
-    public Button ScienceButton;
-    public Button EconomyButton;
-    public Button EntertainmentButton;
+    public Button sportsButton;
+    public Button politicsButton;
+    public Button techButton;
+    public Button scienceButton;
+    public Button economyButton;
+    public Button entertainmentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
-        // Set up the login form.
+
+        sportsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intent = new Intent(CategoryActivity.this, NewsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
 
