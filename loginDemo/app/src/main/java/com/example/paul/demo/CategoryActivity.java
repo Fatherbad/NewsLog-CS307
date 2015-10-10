48972c59dd6261bf921dc1bf7213a793ed9055c3
Joseph R. Nathan <nathan@pod2-4.cs.purdue.edu>
2015-10-10 12:56:42 -0400

    activity_category added

---
+++ loginDemo/app/src/main/java/com/example/paul/demo/CategoryActivity.java
@@ -0,0 +1,10 @@
+package com.example.paul.demo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nathan on 10/10/15.
+ */
+public class CategoryActivity extends AppCompatActivity{
+
+}
