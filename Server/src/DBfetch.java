9b31835bcd0b446b1f49336fcbcd93eda63a052c
Paul Heldring <pheldrin@purdue.edu>
2015-10-15 17:31:48 -0400

    Server connects to DB and sends url to phone

--- Server/src/DBfetch.java
+++ Server/src/DBfetch.java
@@ -16,42 +16,42 @@ import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import org.json.JSONArray;
 
 public class DBfetch {
-	
 
-	static  JSONObject get (String catogory) throws ClientProtocolException, IOException, JSONException{
-		
-		 ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-		 nameValuePairs.add(new BasicNameValuePair("category", catogory));
-		 HttpParams httpParameters = new BasicHttpParams();
-		 HttpConnectionParams.setConnectionTimeout(httpParameters, 15000);
-		 HttpConnectionParams.setSoTimeout(httpParameters, 15000);	
-		 HttpClient httpclient = new DefaultHttpClient(httpParameters);
-		 HttpPost httppost = new HttpPost("http://10.184.223.128/newslog/serviceSetArticles.php");
-		 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-		 HttpResponse response = httpclient.execute(httppost);
-		 HttpEntity entity = response.getEntity();
-		 String result = EntityUtils.toString(entity);
-		 JSONObject ret = new JSONObject(result);
-		 System.out.println("rrrrrrrrrr" + result);
-		 return ret;
-		 
-	}
+
+    static  JSONArray get (String catogory,ArrayList <NameValuePair> nameValuePairs) throws ClientProtocolException, IOException, JSONException{
+
 	
-	static void update (String user, String password) throws ClientProtocolException, IOException{
-	
-		 ArrayList<NameValuePair> nameValuePairs =  new ArrayList<NameValuePair>();
-		 nameValuePairs.add(new BasicNameValuePair("username", user));	
-		 nameValuePairs.add(new BasicNameValuePair("password", password));
-		 HttpParams httpParameters = new BasicHttpParams();
-		 HttpConnectionParams.setConnectionTimeout(httpParameters, 15000);
-		 HttpConnectionParams.setSoTimeout(httpParameters, 15000);	
-		 HttpClient httpclient = new DefaultHttpClient(httpParameters);
-		 HttpPost httppost = new HttpPost("http://10.184.223.128/newslog/serviceSetArticles.php");
-		 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-		 HttpResponse response = httpclient.execute(httppost);
-	
-	}
+	nameValuePairs.add(new BasicNameValuePair("category", catogory));
+	HttpParams httpParameters = new BasicHttpParams();
+	HttpConnectionParams.setConnectionTimeout(httpParameters, 15000);
+	HttpConnectionParams.setSoTimeout(httpParameters, 15000);  
+	HttpClient httpclient = new DefaultHttpClient(httpParameters);
+	HttpPost httppost = new HttpPost("http://10.184.223.128/newslog/serviceGetArticles.php");
+	httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+	HttpResponse response = httpclient.execute(httppost);
+	HttpEntity entity = response.getEntity();
+	String result = EntityUtils.toString(entity);
+	JSONArray ret = new JSONArray(result);
+	System.out.println("rrrrrrrrrr" + result);
+	return ret;
+
+    }
+
+    static void update (String user, String password, ArrayList <NameValuePair> nameValuePairs) throws ClientProtocolException, IOException{
+
+	//ArrayList<NameValuePair> nameValuePairs =  new ArrayList<NameValuePair>();
+	nameValuePairs.add(new BasicNameValuePair("username", user));  
+	nameValuePairs.add(new BasicNameValuePair("password", password));
+	HttpParams httpParameters = new BasicHttpParams();
+	HttpConnectionParams.setConnectionTimeout(httpParameters, 15000);
+	HttpConnectionParams.setSoTimeout(httpParameters, 15000);	
+	HttpClient httpclient = new DefaultHttpClient(httpParameters);
+	HttpPost httppost = new HttpPost("http://10.184.223.128/newslog/serviceSetUser.php");
+	httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+	HttpResponse response = httpclient.execute(httppost);
+	System.out.println("serverris workinddddddddddd");
+    }
 }
